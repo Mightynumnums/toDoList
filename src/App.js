957a9631c03230f8e59fa52e31ff0ad60adeedbe
Mightynumnums <deadcookies@gmail.com>
2018-10-29 15:31:34 -0400

    Break action reducers and reducers into different files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { connect } from 'react-redux'
+import { updateUser } from './actions/user-actions'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.onUpdateUser = this.onUpdateUser.bind(this)
+  }
+  onUpdateUser(event) {
+    this.props.onUpdateUser(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <input onChange={this.onUpdateUser} />
+        {this.props.user}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  products: state.products,
+  user: state.user
+})
+
+const mapDispatchToProps = {
+  onUpdateUser: updateUser
+
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
