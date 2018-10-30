a9584bca30d58108c607992a6bc7df6120869eaa
Mightynumnums <deadcookies@gmail.com>
2018-10-30 11:31:42 -0400

    Add actions, reducers and add provider and store to index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer)
+//import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 
+//compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(rootReducer)
 
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <App />, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
